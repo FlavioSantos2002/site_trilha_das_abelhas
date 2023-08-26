86d97f2cb22bec00a1c77a3f587f914748417845
FlavioSantos2002 <luizflavio.20052002@gmail.com>
2023-08-26 03:25:00 -0400

    add

---
+++ script/script1.js
@@ -0,0 +1,33 @@
+
+// Crie um elemento de imagem
+const imgElement = document.createElement('img');
+
+// Defina os atributos da imagem
+imgElement.src = '/imagens/imagens_geral/mel.png';
+imgElement.alt = 'Imagem Adicionada com JavaScript';
+
+// Adicione a classe para estilo (opcional)
+imgElement.classList.add('centered-image4');
+
+// Encontre o elemento de contêiner onde deseja adicionar a imagem
+const imageContainer = document.getElementById('teste');
+
+
+var botao = document.querySelector("#casa")
+var botao2 = document.querySelector("#flor")
+
+
+if(botao2.addEventListener("mouseover", function(){
+    botao.addEventListener("click", function(){
+        // Adicione a imagem ao contêiner
+        imageContainer.appendChild(imgElement);
+        setTimeout(function() {
+            imgElement.remove();
+          }, 1000);
+    })
+}));
+
+
+
+
+
